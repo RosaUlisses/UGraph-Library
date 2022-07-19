86cd03a909b60b86fa893cfae2e83a3ef4fbc7a6
RosaUlisses <bolissesrosa@gmail.com>
2022-07-18 23:25:39 -0300

    add graph interface

---
+++ GraphLib/GraphLib/IGraph.cs
@@ -0,0 +1,15 @@
+﻿namespace GraphLib
+{
+    public interface IGraph<TVertex>
+    {
+        List<TVertex> BreadthFirstSearch(TVertex source);
+        List<TVertex> BreadthFirstSearch(TVertex source, TVertex destination);
+        List<TVertex> DepthFirstSearch(TVertex source);
+        List<TVertex> DepthFirstSearch(TVertex source, TVertex destination);
+        List<TVertex> TopologicalSort(TVertex source);
+        List<TVertex> Djikstra(TVertex source, TVertex destination);
+        List<TVertex> BellmanFord(TVertex source, TVertex destination);
+        List<TVertex> AStar(TVertex source, TVertex destination);
+    }   
+}
+
